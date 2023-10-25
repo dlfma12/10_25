85ff69e91019dd58fba5fcc5475f8fb980890750
2302595-조나영 <133933552+dlfma12@users.noreply.github.com>
2023-10-25 12:41:05 +0900

    Add files via upload

--- 10_25 실습.js
+++ 10_25 실습.js
@@ -48,6 +48,60 @@ console.log(fruits);
 // splice: remove an item by index position
 fruits.push('apple', 'banana', 'orange')
 console.log(fruits);
+fruits.splice(1, 1);
+console.log(fruits);
+fruits.splice(1, 1, 'apple', 'banana')
+console.log(fruits);
+
+// combine two arrays
+const fruits2 = ['apple', 'banana']
+const newFruits = fruits.concat(fruits2);
+console.log(newFruits);
+
+// Q1. make a string out of an array
+{
+    const fruits = ['apple', 'banana', 'orange'];
+    const result = fruits.join(',');
+    console.log(result);
+}
+
+// Q2. make an array out of a string
+{
+    const fruits = 'apple, banana, orange';
+}
+
+// Q3. make this array look like this: [5, 4, 3, 2, 1]
+{
+    const array = [1, 2, 3, 4, 5];
+}
+
+// Q4. make new array without the first two elements
+{
+    const array = [1, 2, 3, 4, 5];
+}
+
+// 04. make new array without the first two elements
+{
+    const array = [1, 2, 3, 4, 5];
+    const result = array.slice(2, 5);
+    console.log(result);
+    console.log(array);
+}
+class Student {
+    constructor(name, age, enrolled, score)  {
+        this.name = name;
+        this.age = age;
+        this.enrolled = enrolled;
+        this.score = score;
+    }
+}
+const students = [
+    new Student('A', 29, true, 45),
+    new Student('B', 28, false, 80),
+    new Student('C', 30, true, 90),
+    new Student('D', 50, false, 66),
+    new Student('E', 18, true, 88)
+];
 
 
 
